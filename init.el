ce3186fa1abfb2ee3aaa207db973f459c907f69d
Louise Zanier <louise.zanier@getalma.eu>
2023-01-13 11:52:47 +0100

    add all-the-icons-completion

--- init.el
+++ init.el
@@ -28,6 +28,12 @@
 (use-package all-the-icons
   :if (display-graphic-p))
 
+(use-package all-the-icons-completion
+  :after (marginalia all-the-icons)
+  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :init
+  (all-the-icons-completion-mode))
+
 (use-package doom-themes
   :config
   (setq doom-themes-enable-italic t)
