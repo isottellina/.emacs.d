4fcde85659b500869b2a8e3cb93c5d8f70426952
Louise Zanier <louise.zanier@getalma.eu>
2023-12-31 16:45:15 +0100

    switch to pylsp for python LSP

--- modules/tools/init-lsp.el
+++ modules/tools/init-lsp.el
@@ -1,6 +1,8 @@
 (use-package eglot
   :commands (eglot eglot-ensure)
-  :config (setf (alist-get '(python-mode python-ts-mode) eglot-server-programs nil nil #'equal)
-		'("pyright-langserver" "--stdio")))
+  :config
+  (setf (alist-get '(python-mode python-ts-mode) eglot-server-programs nil nil #'equal)
+	'("pylsp"))
+  (setq-default eglot-workspace-configuration '(:pylsp (:plugins (:rope_autoimport (:enabled t))))))
 
 (provide 'init-lsp)
